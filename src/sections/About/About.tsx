7295e7e55d804ddc49c1eed5b55b0b9723611429
naimishverma17 <itakgec@gmail.com>
2022-04-21 16:41:54 +0530

    chore: about section responsive

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -7,9 +7,9 @@ import AboutImage from '../../images/about.svg';
 const About: React.FC = (props: any) => {
     return (
         <SectionLayout title='About'>
-            <div className='d-flex align-items-center justify-content-between'>
+            <div className='d-flex flex-column flex-md-row align-items-center'>
                 <img src={AboutImage} alt='about'/>
-                <p className='about w-75'>
+                <p className='about w-75 mt-5 mt-md-0 ms-md-5'>
                     <span className='title'>E</span>voqys is the expert partner you need to deliver innovative, scalable, and competitive results. <br/>
                     We are a group of tech enthusiasts leading the digital transformation of our worldwide clients through our services. We have our own Youtube channel where we share a glimpse of our knowledge.
                 </p>

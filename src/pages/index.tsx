78afa3f890ea0fa8358b5a41840d90fe200e4b29
naimishverma17 <itakgec@gmail.com>
2022-04-22 10:33:19 +0530

    feat: added our process section structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import '../styles/main.scss';
 import Header from '../sections/Header/Header';
 import Landing from '../sections/Landing/Landing';
 import About from '../sections/About/About';
+import OurProcess from '../sections/OurProcess/OurProcess';
 
 // markup
 const IndexPage = () => {
@@ -11,6 +12,7 @@ const IndexPage = () => {
             <Header/>
             <Landing/>
             <About/>
+            <OurProcess/>
         </div>
     );
 };
---
+++ src/sections/OurProcess/OurProcess.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SectionLayout from '../../components/SectionLayout';
+
+const OurProcess:React.FC = (props: any) => {
+    return (
+        <SectionLayout title='Our process'>
+            <p>
+                We strictly follow the complete Software development lifecycle to ensure a single vision of a product for all the parties involved and better management control over the entire project.
+            </p>
+        </SectionLayout>
+     );
+};
+
+export default OurProcess;

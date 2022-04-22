c4f37d25321ce0a9ce656d8a378a59b752fa8616
naimishverma17 <itakgec@gmail.com>
2022-04-22 10:35:44 +0530

    chore: added our process image

--- src/sections/OurProcess/OurProcess.tsx
+++ src/sections/OurProcess/OurProcess.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SectionLayout from '../../components/SectionLayout';
+// @ts-ignore
+import OurProcessImage from '../../images/our-process.svg';
 
 const OurProcess:React.FC = (props: any) => {
     return (
@@ -7,6 +9,9 @@ const OurProcess:React.FC = (props: any) => {
             <p>
                 We strictly follow the complete Software development lifecycle to ensure a single vision of a product for all the parties involved and better management control over the entire project.
             </p>
+            <div className='d-flex align-items-md-center'>
+                <img src={OurProcessImage} alt='Our process' className='img-fluid'/>
+            </div>
         </SectionLayout>
      );
 };

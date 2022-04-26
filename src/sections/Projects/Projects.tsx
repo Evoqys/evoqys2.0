c25ed619ef6206a02d44b77fedce375e0d24f1d1
naimishverma17 <itakgec@gmail.com>
2022-04-26 10:44:57 +0530

    feat: added tech icons

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import './Projects.scss';
 import SectionLayout from '../../components/SectionLayout';
+// @ts-ignore
+import Sbys from '../../images/sbys.png';
+
+type ProjectImage = { image: string, label: string };
+type Project = { image: string, description: string, tech: ProjectImage[] };
+
+const PROJECTS: Project[] = [
+    {
+        image: Sbys,
+        description: 'StandBYStand is a digital platform to bring together the freelancers of the stand building industry and potential clients. Our main role was to add more modules to their existing code base.',
+        tech: [
+            { image: '', label: '' }
+        ]
+    }
+];
 
 const Projects: React.FC = (props: any) => {
     return (

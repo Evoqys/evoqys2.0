e88fd64b3e4f211f95e0fd5d809df2bfd60695c4
naimishverma17 <itakgec@gmail.com>
2022-05-03 10:21:21 +0530

    fix: added my

--- src/components/SectionLayout.tsx
+++ src/components/SectionLayout.tsx
@@ -3,7 +3,7 @@ import Title from './Title/Title';
 
 const SectionLayout:React.FC<{title:string, id:string}> = (props: any) => {
     return (
-        <div className='pt-5' id={props.id}>
+        <div className='pt-5 my-5' id={props.id}>
             <Title>{props.title}</Title>
             {props.children}
         </div>
